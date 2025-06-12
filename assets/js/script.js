452b8b35640a6b533ec357ee5ac28a47bce3241d
BMileham98 <brittanymmileham@gmail.com>
2025-06-12 17:59:11 +0100

    calculateCorrectAnswer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for(let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -26,7 +26,15 @@ function runGame(gameType){
 }
 
 function checkAnswer() {
-
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+    runGame(calculatedAnswer[1]); 
 }
 
 function calculateCorrectAnswer() {
