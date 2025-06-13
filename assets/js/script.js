ee8c79443bbf17886e1a5ae26f6bf85306f00bbc
BMileham98 <brittanymmileham@gmail.com>
2025-06-13 11:59:25 +0100

    division work now I hope?

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,7 +66,7 @@ function calculateCorrectAnswer() {
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"]; 
     } else if (operator === "/") {
-        return [operand1, "division"];
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator: ${operator}`);
         throw `Unimplemented operator: ${operator}. Aborting!`;
